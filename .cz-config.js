5e068fd514b3d1232cbb66f56ba16f86149b041e
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-12-08 15:33:41 +0900

    ff

--- .cz-config.js
+++ .cz-config.js
@@ -16,6 +16,6 @@ module.exports = {
   },
   allowCustomScopes: false,
   allowBreakingChanges: ["feat", "fix"],
-  skipQuestions: ["footer"],
+  skipQuestions: ["scope", "footer"],
   subjectLimit: 100,
 };
