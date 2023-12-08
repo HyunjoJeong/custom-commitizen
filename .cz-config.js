f2ed0350e571259f523bf020cfb7f39030f1e219
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-12-08 12:28:23 +0900

    ✨ Feat(custom): commit

--- .cz-config.js
+++ .cz-config.js
@@ -1,76 +1,27 @@
 module.exports = {
   types: [
-    { value: "feat", name: "feat:     A new feature" },
-    { value: "fix", name: "fix:      A bug fix" },
-    { value: "docs", name: "docs:     Documentation only changes" },
+    { value: "✨ Feat", name: "✨ Feat:\tAdd a new feature" },
+    { value: "🐛 Modify", name: "🐛 Fix:\tModify production, UI,UX code" },
+    { value: "📝 Docs", name: "📝 Docs:\tAdd or update documentation" },
     {
-      value: "style",
-      name: "style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)",
+      value: "💄 Style",
+      name: "💄 Style:\tAdd or update code format (not updation production, UI,UX code)",
     },
     {
-      value: "refactor",
-      name: "refactor: A code change that neither fixes a bug nor adds a feature",
+      value: "🤖 Refactor",
+      name: "🤖 Refactor:\tCode change that neither fixes a bug nor adds a feature",
     },
     {
-      value: "perf",
-      name: "perf:     A code change that improves performance",
+      value: "✅ Test",
+      name: "✅ Test:\tCode change related with tests cases",
     },
-    { value: "test", name: "test:     Adding missing tests" },
     {
-      value: "chore",
-      name: "chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation",
+      value: "🚚 Chore",
+      name: "🚚 Chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation",
     },
-    { value: "revert", name: "revert:   Revert to a commit" },
-    { value: "WIP", name: "WIP:      Work in progress" },
   ],
-
-  scopes: [
-    { name: "accounts" },
-    { name: "admin" },
-    { name: "exampleScope" },
-    { name: "changeMe" },
-  ],
-
-  usePreparedCommit: false, // to re-use commit from ./.git/COMMIT_EDITMSG
-  allowTicketNumber: false,
-  isTicketNumberRequired: false,
-  ticketNumberPrefix: "TICKET-",
-  ticketNumberRegExp: "\\d{1,5}",
-
-  // it needs to match the value for field type. Eg.: 'fix'
-  /*
-  scopeOverrides: {
-    fix: [
-
-      {name: 'merge'},
-      {name: 'style'},
-      {name: 'e2eTest'},
-      {name: 'unitTest'}
-    ]
-  },
-  */
-  // override the messages, defaults are as follows
-  messages: {
-    type: "Select the type of change that you're committing:",
-    scope: "\nDenote the SCOPE of this change (optional):",
-    // used if allowCustomScopes is true
-    customScope: "Denote the SCOPE of this change:",
-    subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
-    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-    breaking: "List any BREAKING CHANGES (optional):\n",
-    footer:
-      "List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n",
-    confirmCommit: "Are you sure you want to proceed with the commit above?",
-  },
-
-  allowCustomScopes: true,
+  allowCustomScopes: false,
   allowBreakingChanges: ["feat", "fix"],
-  // skip any questions you want
-  // skipQuestions: ['scope', 'body'],
-
-  // limit subject length
+  skipQuestions: ["body"],
   subjectLimit: 100,
-  // breaklineChar: '|', // It is supported for fields body and footer.
-  // footerPrefix : 'ISSUES CLOSED:'
-  // askForBreakingChangeFirst : true, // default is false
 };
