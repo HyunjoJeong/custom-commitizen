8de6de7efe9fcf1d834413b1bc15f6e061204286
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-12-08 14:18:58 +0900

    ✨ feat(custom): config update

--- .cz-config.js
+++ .cz-config.js
@@ -1,29 +1,18 @@
 module.exports = {
   types: [
-    { value: "✨ feat", name: "✨ feat:\tAdd a new feature" },
-    { value: "🐛 modify", name: "🐛 fix:\tModify production, UI,UX code" },
-    { value: "📝 docs", name: "📝 docs:\tAdd or update documentation" },
-    {
-      value: "💄 style",
-      name: "💄 style:\tAdd or update code format (not updation production, UI,UX code)",
-    },
-    {
-      value: "🤖 refactor",
-      name: "🤖 refactor:\tCode change that neither fixes a bug nor adds a feature",
-    },
-    {
-      value: "✅ rest",
-      name: "✅ rest:\tCode change related with tests cases",
-    },
-    {
-      value: "🚚 chore",
-      name: "🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation",
-    },
+    { value: "✨ feat", name: "✨ feat:\tNew feature developed" },
+    { value: "🐛 fix", name: "🐛 fix:\tBug fix or QA" },
+    { value: "📝 docs", name: "📝 docs:\tUpdate documentation" },
+    { value: "💄 style", name: "💄 style:\tCss change" },
+    { value: "🤖 refactor", name: "🤖 refactor:\tCode refactoring" },
+    { value: "✅ test", name: "✅ test:\tCode change for tests cases" },
+    { value: "🚚 chore", name: "🚚 chore:\tSetup auxiliary tools" },
   ],
   messages: {
     type: "Select type of commit:",
     subject: "commit message:",
     body: "extra commit message:",
+    confirmCommit: "Press Enter to proceed Commit (n key to abort)",
   },
   allowCustomScopes: false,
   allowBreakingChanges: ["feat", "fix"],
