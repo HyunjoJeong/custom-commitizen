c8db4253df29a70c0c799f90be4b1bc7576172d5
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-12-08 14:23:39 +0900

    ✨ feat(custom): config 수정

--- .cz-config.js
+++ .cz-config.js
@@ -1,17 +1,17 @@
 module.exports = {
   types: [
-    { value: "✨ feat", name: "✨ feat:\tNew feature developed" },
+    { value: "✨ feat", name: "✨ feat:\tNew feature" },
     { value: "🐛 fix", name: "🐛 fix:\tBug fix or QA" },
-    { value: "📝 docs", name: "📝 docs:\tUpdate documentation" },
     { value: "💄 style", name: "💄 style:\tCss change" },
     { value: "🤖 refactor", name: "🤖 refactor:\tCode refactoring" },
+    { value: "📝 docs", name: "📝 docs:\tUpdate documentation" },
     { value: "✅ test", name: "✅ test:\tCode change for tests cases" },
     { value: "🚚 chore", name: "🚚 chore:\tSetup auxiliary tools" },
   ],
   messages: {
     type: "Select type of commit:",
     subject: "commit message:",
-    body: "extra commit message:",
+    body: "extra commit message: (Optional)",
     confirmCommit: "Press Enter to proceed Commit (n key to abort)",
   },
   allowCustomScopes: false,
