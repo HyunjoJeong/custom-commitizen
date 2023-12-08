3b7ad781473fc0f2e7bea15894861a99cd44a5c7
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-12-08 14:26:17 +0900

    ✨ feat(custom): config 수정

--- .cz-config.js
+++ .cz-config.js
@@ -11,7 +11,7 @@ module.exports = {
   messages: {
     type: "commit type:",
     subject: "commit message:",
-    body: "extra commit message: (Optional)",
+    body: "extra commit message (optional):",
     confirmCommit: "press Enter to proceed commit (n key to abort)",
   },
   allowCustomScopes: false,
